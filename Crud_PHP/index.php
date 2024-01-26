0702fea67f6b1eb645e6dfa9b66f2f60e9023610
Gui-hornig <guihornig@gmail.com>
2024-01-26 15:28:15 -0300

    Update index.php

--- Crud_PHP/index.php
+++ Crud_PHP/index.php
@@ -14,7 +14,7 @@
         <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-
+    
 
 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
