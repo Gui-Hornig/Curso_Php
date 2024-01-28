b66b4fe52956dbb593c03e8d5b96768a626fbec0
Gui-hornig <guihornig@gmail.com>
2024-01-27 22:20:59 -0300

    Update index.php

--- Crud_PHP/index.php
+++ Crud_PHP/index.php
@@ -13,6 +13,7 @@
         <a class="navbar-brand" href="#">Cadastro</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
+            
         </button>
     
 
